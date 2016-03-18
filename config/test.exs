8b6e676fc339463f9b967412d80520f8f581b353
张顶炜 <zhangdingwei@souche.com>
2016-03-18 14:07:28 +0800

    support ecto 2.0

--- config/test.exs
+++ config/test.exs
@@ -3,8 +3,10 @@ use Mix.Config
 config :scrivener, Scrivener.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
+  username: "doom_test",
+  password: "doom_test",
   database: "scrivener_test",
-  username: System.get_env("SCRIVENER_DB_USER") || System.get_env("USER")
+  hostname: "localhost"
 
 config :logger, :console,
   level: :error
